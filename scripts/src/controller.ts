65abb7057e9db4841fa9720d66586fb2ead01dab
Vincent Sargeni <vsargeni@wifi-roaming-128-4-135-5.host.udel.edu>
2019-10-25 14:12:25 -0400

    Initally fleshing out API calls

--- scripts/src/controller.ts
+++ scripts/src/controller.ts
@@ -5,6 +5,43 @@ export class Controller {
         res.send("Hello World");
     }
     public postHello(req: express.Request, res: express.Response): void {
-        res.send(req.body);
+        //console.log(req.params.userid)
+        res.send(req.params.userid);
+    }
+
+    public getAllTrains(req: express.Request, res: express.Response){
+        //return list of all trains
+    }
+
+    public getTrain(req: express.Request, res: express.Response){
+        //return train based on Train ID
+    }
+
+    public getUser(req: express.Request, res: express.Response){
+        //return user info
+    }
+
+    public postCreateUser(req: express.Request, res: express.Response){
+        //return success, add user
+    }
+
+    public putUpdateCustomer(req: express.Request, res: express.Response){
+        //return success, update customer
+    }
+
+    public putUpdateTicket(req: express.Request, res: express.Response){
+        //update seat, return success/ failure
+    }
+
+    public getTickets(req: express.Request, res: express.Response){
+        //return list of seats
+    }
+
+    public getRoutes(req: express.Request, res: express.Response){
+        //return routes
+    }
+
+    public getStations(req: express.Request, res: express.Response){
+        //return stations
     }
 }
