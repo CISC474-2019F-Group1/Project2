c1f5610a7865070c6b605037d9e60f15b8a1217b
302Blue <abaldwin@udel.edu>
2019-10-30 10:57:23 -0400

    Changes

--- scripts/src/controller.ts
+++ scripts/src/controller.ts
@@ -9,39 +9,39 @@ export class Controller {
         res.send(req.params.userid);
     }
 
-    public getAllTrains(req: express.Request, res: express.Response){
+    public getAllTrains(req: express.Request, res: express.Response) {
         //return list of all trains
     }
 
-    public getTrain(req: express.Request, res: express.Response){
+    public getTrain(req: express.Request, res: express.Response) {
         //return train based on Train ID
     }
 
-    public getUser(req: express.Request, res: express.Response){
+    public getUser(req: express.Request, res: express.Response) {
         //return user info
     }
 
-    public postCreateUser(req: express.Request, res: express.Response){
+    public postCreateUser(req: express.Request, res: express.Response) {
         //return success, add user
     }
 
-    public putUpdateCustomer(req: express.Request, res: express.Response){
+    public putUpdateCustomer(req: express.Request, res: express.Response) {
         //return success, update customer
     }
 
-    public putUpdateTicket(req: express.Request, res: express.Response){
+    public putUpdateTicket(req: express.Request, res: express.Response) {
         //update seat, return success/ failure
     }
 
-    public getTickets(req: express.Request, res: express.Response){
+    public getTickets(req: express.Request, res: express.Response) {
         //return list of seats
     }
 
-    public getRoutes(req: express.Request, res: express.Response){
+    public getRoutes(req: express.Request, res: express.Response) {
         //return routes
     }
 
-    public getStations(req: express.Request, res: express.Response){
+    public getStations(req: express.Request, res: express.Response) {
         //return stations
     }
 }
