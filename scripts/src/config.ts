0de1d2bfdf1ea32043756cad1fddc3a6d84d6213
karlstomberg <karlstom@udel.edu>
2019-12-01 17:53:53 -0500

    Fixed config

--- scripts/src/config.ts
+++ scripts/src/config.ts
@@ -3,7 +3,7 @@ export namespace Config {
     // Secret key for JWT signing and encryption
     export const secret = process.env.secret || "super secret passphrase";
     // Database connection information
-    export const database = "mongodb://vsargeni:smitty@35.231.61.192:27017/trainsDB";
+    export const database = process.env.database;
     // Setting port for server
     export const port = +process.env.serverPort || 3000;
 }
